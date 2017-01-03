d4de0ee2adafe60fb0c08efcf3cc410ef4f21a6e
Alex Wood <awood@redhat.com>
2017-01-03 16:11:49 -0500

    Modify livereload.js injection to maintain same-origin.

--- lib/jekyll/commands/serve/servlet.rb
+++ lib/jekyll/commands/serve/servlet.rb
@@ -92,23 +92,29 @@ module Jekyll
         end
 
         def template
+          # Unclear what "snipver" does. Doc at
+          # https://github.com/livereload/livereload-js states that the recommended
+          # setting is 1.
+
+          # Complicated JavaScript to ensure that livereload.js is loaded from the same origin as the
+          # page.  Mostly useful for dealing with the browser's distinction between 'localhost' and 127.0.0.1
           template = <<-TEMPLATE
-          <script>
-            document.write('<script src="<%= livereload_source %>"></' + 'script>');
+          <script>            
+            document.write(
+              '<script src="http://' +
+              (location.host || 'localhost').split(':')[0] +
+              ':<%=@options["livereload_port"] %>/livereload.js?snipver=1<%= livereload_args %>"' +
+              '></' +
+              'script>');
           </script>
           TEMPLATE
           ERB.new(Jekyll::Utils.strip_heredoc(template))
         end
 
-        def livereload_source
-          # Unclear what "snipver" does. Doc at
-          # https://github.com/livereload/livereload-js state that the recommended
-          # setting is 1.
-          src = "http://#{host_to_use}:#{@options["livereload_port"]}"\
-            "/livereload.js?snipver=1"
-
+        def livereload_args
           # XHTML standard requires ampersands to be encoded as entities when in
           # attributes. See http://stackoverflow.com/a/2190292
+          src = ''
           if @options["livereload_min_delay"]
             src << "&amp;mindelay=#{@options["livereload_min_delay"]}"
           end
